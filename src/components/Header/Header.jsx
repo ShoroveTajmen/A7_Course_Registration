dd71a05c46159bd0a53df413ae11f76500d11e32
ShoroveTajmen <sorovitajmen@gmail.com>
2023-09-14 20:23:00 +0600

    header part done

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div>
+      <div className="w-[1440px] mx-auto">
+        <h1 className="font-bold text-4xl text-center mt-[50px]">Course Registration</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
