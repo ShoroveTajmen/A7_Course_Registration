4096fba1bdbbb37d88ce2a1ae3e5d83db5fbeddc
ShoroveTajmen <sorovitajmen@gmail.com>
2023-09-14 23:28:29 +0600

    Cart design part done

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,20 @@
 const Cart = () => {
-    return (
-        <div className="w-1/3">
-            <h1>Course Name</h1>
-        </div>
-    );
+  return (
+    <div className="w-1/4 bg-white mx-auto rounded-xl h-[500px] mt-5">
+      {" "}
+      <div className=" p-8">
+        <h1 className="font-semibold text-[#2F80ED] text-lg mb-[16px]">
+          Credit Hour Remaining 7 hr
+        </h1>
+        <hr className="mb-[16px]" />
+        <p className="font-semibold text-xl mb-[16px]">Course Name</p>
+        <hr className="mb-[16px]" />
+        <h1 className="mb-[16px]">Total Credit Hour : 13</h1>
+        <hr className="mb-[16px]" />
+        <h1>Total Price : 48000 USD</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
